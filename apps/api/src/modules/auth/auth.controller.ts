fbde7bcc862149e67ea1d069c06ea2efd3d1dc73
matshp0 <matvii.shpyruk@gmail.com>
2025-11-08 14:21:30 +0200

    feat(server): add auth module

---
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { Public } from 'src/common/decorators/public';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+@Controller('/auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Public()
+  @Post('/login')
+  async login(@Body() dto: LoginDto, @Res() res: FastifyReply) {
+    const [refreshToken, accessToken] = await this.authService.login(dto);
+    res.setCookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      path: '/auth',
+      secure: true,
+      sameSite: 'strict',
+      maxAge: this.configService.get<number>('jwt.refreshTtl')! / 1000,
+    });
+    return res.send({ accessToken });
+  }
+
+  @Public()
+  @Get('/refresh')
+  async refresh(@Req() req: FastifyRequest) {
+    return await this.authService.refresh(req.cookies.refresh_token!);
+  }
+}
---
+++ apps/api/src/modules/auth/auth.service.ts
@@ -0,0 +1,60 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from 'src/data/repositories/user.repository';
+import { LoginDto } from './dto/login.dto';
+import { compare } from 'bcrypt';
+import { RefreshToken } from './types/refresh.token';
+import { User } from 'prisma/generated';
+import { AccessToken } from './types/accessToken.type';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async login(payload: LoginDto) {
+    const user = await this.userRepository.findByEmail(payload.login);
+    if (!user) throw new NotFoundException('User not found');
+    const isCorrect = await compare(payload.password, user.passwordHash);
+    if (!isCorrect) throw new UnauthorizedException('Wrong email or password');
+    const result = await Promise.all([
+      this.generateRefreshToken(user),
+      this.generateAccessToken(user),
+    ]);
+    return result;
+  }
+
+  async refresh(refreshToken?: string) {
+    if (!refreshToken)
+      throw new BadRequestException('No refresh token provided');
+    const payload: RefreshToken | AccessToken =
+      await this.jwtService.verifyAsync(refreshToken);
+    if (payload.type !== 'refresh')
+      throw new UnauthorizedException('Invalid refresh token');
+    const user = await this.userRepository.findById(payload.sub);
+    if (!user) throw new NotFoundException('User not found');
+    const accessToken = await this.generateAccessToken(user);
+    return { accessToken };
+  }
+
+  private async generateRefreshToken(user: User): Promise<string> {
+    const payload: RefreshToken = { sub: user.id, type: 'refresh' };
+    const ttl = this.configService.get<number>('jwt.refreshTtl');
+    return await this.jwtService.signAsync(payload, { expiresIn: ttl });
+  }
+
+  private async generateAccessToken(user: User): Promise<string> {
+    const payload: AccessToken = { sub: user.id, type: 'access' };
+    const ttl = this.configService.get<number>('jwt.ttl');
+    return await this.jwtService.signAsync(payload, { expiresIn: ttl });
+  }
+}
