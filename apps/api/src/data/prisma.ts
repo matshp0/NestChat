d3f147e45ef11dae09135bac9f79e9386e1e3504
matshp0 <matvii.shpyruk@gmail.com>
2026-01-15 20:13:35 +0200

    feat: add graceful exit for prisma provider

--- apps/api/src/data/prisma.ts
+++ apps/api/src/data/prisma.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  type OnModuleInit,
+  type OnModuleDestroy,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'prisma/generated';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor(private readonly configService: ConfigService) {
     const connectionString = configService.getOrThrow<string>('db');
-    const adapter = new PrismaPg({ connectionString });
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
